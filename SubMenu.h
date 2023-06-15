1084cba6347146ae8499e4de313f6aa5232ecab5
sebe324 <sebeq05@gmail.com>
2023-06-15 08:25:28 +0200

    Added an overlay for game over and pause

--- SubMenu.h
+++ SubMenu.h
@@ -6,8 +6,8 @@
 #include <SFML/Graphics.hpp>
 #include "noise/perlinNoise.h"
 
-// This class was created to produce the game over and the ESC key screens. Since in both cases we have the creation of a menu, I use 
-// a single class for both of them. The class is an altered copy of the Menu class, but in an attempt do decrease the amount of 
+// This class was created to produce the game over and the ESC key screens. Since in both cases we have the creation of a menu, I use
+// a single class for both of them. The class is an altered copy of the Menu class, but in an attempt do decrease the amount of
 // changes to the original code, and due to my lack of familiarity with it, I chose to simply create a new class.
 // The way I separated the ESC key menu from the game over one is not object oriented, so that is one way to improve this code.
 
@@ -20,6 +20,7 @@ class SubMenu : public sf::Drawable {
         Button bExitGame;
         Button bSettings;
 
+        sf::RectangleShape overlay;
         SubMenu(std::string fontPath, std::string texturePath, std::string menu_type);
         void update(sf::Vector2f pos);
 
